d9d71affd5d8dc72ed6497997acfcd90f1be2500
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-04-30 12:46:11 -0300

    testes unitarios inicio

--- src/data.js
+++ src/data.js
@@ -1,21 +1,23 @@
 // funções de exemplo
 
-export const example = () => {
-  return 'example';
-};
-
-export const anotherExample = () => {
-  return 'OMG';
-};
-
-export const filterData = () => {
-  return 'ok filtrado';
-};
-
-export const sortData = () => {
-  return 'ok filtrado'
-};
-
-export const computeStats = () => {
-  return 'ok filtrado'
+export const data = {
+  example: () => {
+    return 'example';
+  },
+  anotherExample: () => {
+    return 'OMG';
+  },
+  filterData: (data) => {
+    if (!data) {
+      throw TypeError('Hello');
+    }
+    return 'ok filtrado';
+  },
+  sortData: () => {
+    return 'ok filtrado'
+  },
+  computeStats: () => {
+    return 'ok filtrado'
+  }
 }
+
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,46 +1,46 @@
-import { example, anotherExample, filterData, sortData, computeStats} from '../src/data.js';
+import { data } from '../src/data.js';
 
 
 describe('example', () => {
   it('is a function', () => {
-    expect(typeof example).toBe('function');
+    expect(typeof data.example).toBe('function');
   });
 
   it('returns `example`', () => {
-    expect(example()).toBe('example');
+    expect(data.example()).toBe('example');
   });
 });
 
 describe('anotherExample', () => {
   it('is a function', () => {
-    expect(typeof anotherExample).toBe('function');
+    expect(typeof data.anotherExample).toBe('function');
   });
 
   it('returns `anotherExample`', () => {
-    expect(anotherExample()).toBe('OMG');
+    expect(data.anotherExample()).toBe('OMG');
   });
 });
 
 describe('filterData', () => {
   it('is a function', () => {
-    expect(typeof filterData).toBe('function');
+    expect(typeof data.filterData).toBe('function');
   });
   it('arguments types',() =>{
-    expect(() => data.filterData()).toThrow(TypeError);
+    expect( function(){ return data.filterData(); } ).toThrow(TypeError);
     expect(() => data.filterData(0)).toThrow(TypeError);
     expect(() => data.filterData(null,[])).toThrow(TypeError);
     expect(() => data.filterData(0,0)).toThrow(TypeError)
-   })
+  })
 });
 
 describe('sortData', () => {
   it('is a function', () => {
-    expect(typeof sortData).toBe('function');
+    expect(typeof data.sortData).toBe('function');
   });
 });
 
 describe('computeStats', () => {
   it('is a function', () => {
-    expect(typeof computeStats).toBe('function');
+    expect(typeof data.computeStats).toBe('function');
   });
 });
