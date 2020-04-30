fc9abf649f873da77be195e883c8a0ba01b3160d
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-04-30 15:24:39 -0300

    ajuste merge

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,9 +1,4 @@
-<<<<<<< HEAD
-import { example, anotherExample, filterData, sortData, computeStats} from '../src/data.js';
-=======
 import { data } from '../src/data.js';
->>>>>>> d9d71affd5d8dc72ed6497997acfcd90f1be2500
-
 
 describe('example', () => {
   it('is a function', () => {
@@ -43,32 +38,3 @@ describe('sortData', () => {
   });
 });
 
-<<<<<<< HEAD
-describe('filterData', () => {
-  it('is a function', () => {
-    expect(typeof filterData).toBe('function');
-  });
-  it('arguments types',() =>{
-    expect(() => data.filterData()).toThrow(TypeError);
-    expect(() => data.filterData(0)).toThrow(TypeError);
-    expect(() => data.filterData(null,[])).toThrow(TypeError);
-    expect(() => data.filterData(0,0)).toThrow(TypeError)
-   })
-});
-
-describe('sortData', () => {
-  it('is a function', () => {
-    expect(typeof sortData).toBe('function');
-  });
-});
-
-describe('computeStats', () => {
-  it('is a function', () => {
-    expect(typeof computeStats).toBe('function');
-=======
-describe('computeStats', () => {
-  it('is a function', () => {
-    expect(typeof data.computeStats).toBe('function');
->>>>>>> d9d71affd5d8dc72ed6497997acfcd90f1be2500
-  });
-});
