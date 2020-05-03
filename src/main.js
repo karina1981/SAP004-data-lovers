7acbd5c59c78c4801f190bf5976bccae4acf324c
Jessica Brunhara <brunharaj@gmail.com>
2020-05-03 16:38:42 -0300

    botões pokelist

--- src/main.js
+++ src/main.js
@@ -1,8 +1,41 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
+import { search } from './data.js';
+
 import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
 
-console.log(example, data);
+/*---------------Obs: são só testes dos botoes
+function searchPokemon(){
+  let pokemonBuscado =document.getElementById("search-box").value
+  console.log(pokemonBuscado)
+}
 
+
+function filterType(){
+   let escolhaTipo = document.getElementById("select-type").value
+   console.log(escolhaTipo)
+}
+
+function orderByAlphabet(){
+    let escolhaOrdem = document.getElementById("order-by-alphabet").value
+    console.log(escolhaOrdem)
+}
+----------*/
+
+
+/* BOTÃO BUSCAR*/ 
+document.getElementById("search-box").addEventListener("input", searchPokemon);
+/* FILTRAR TIPO*/ 
+document.getElementById("select-type").addEventListener("change", filterType);
+/* ORDENAR */ 
+document.getElementById("order-by-alphabet").addEventListener("change", orderByAlphabet);
+
+
+
+
+
+
+
+
+
+
+//console.log(example, data);
