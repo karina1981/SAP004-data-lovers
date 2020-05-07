f173b613bdd02c53b85c82db6a2ebf7f16678705
Jessica Brunhara <brunharaj@gmail.com>
2020-05-07 20:12:45 -0300

    estruturando card e imprimindo

--- src/main.js
+++ src/main.js
@@ -2,22 +2,44 @@
 
 import data from './data/pokemon/pokemon.js';
 
-const pokeData = data.pokemon; //pokeData = array
-const rootElement = document.getElementById("root") //importanto div do html
+//pokeData = array. Banco de dados
+const pokeData = data.pokemon;
+const rootElement = document.getElementById("root"); //importanto div do html
 
 
+//estruturando o card
+const pokeCard = function (num, img, name, type, weaknesses) {
+ return `<div class="pokecard">
+            ${num}
+            <img class="miniaturaPokemon" src="${img}" alt ="miniatura pokemon"> 
+            ${name} 
+            Type: ${type} 
+            Weaknesses: ${weaknesses}
+         </div>`
+}
 
-function filterType(){
-   const tipoEscolhido = document.getElementById("select-type").value //pegando o tipo escolhido pelo usuário. const tipoEscolhido recebe.
-   
-   const filteredData = pokeData.filter(function (pokemon){ //const filteredData é onde vai receber o resultado filtrado. pokeData (array com o data.pokemon).filter e escrevo a função que executa o filtro
-     return pokemon.type.includes(tipoEscolhido) //includes compara elementos de outra array, pois type é uma array dentro da array pokeData. 
-   })
+//usando map para gerar o card
+pokeData.map(function (pokemon) {
+  rootElement.innerHTML += pokeCard(pokemon.num,
+                                    pokemon.img, 
+                                    pokemon.name, 
+                                    pokemon.type, 
+                                    pokemon.weaknesses)
+})
+
+
+//função filtrar por tipo
+function filterType() {
+  const tipoEscolhido = document.getElementById("select-type").value //pegando o tipo escolhido pelo usuário. const tipoEscolhido recebe.
+
+  const filteredData = pokeData.filter(function (pokemon) { //const filteredData é onde vai receber o resultado filtrado. pokeData (array com o data.pokemon).filter e escrevo a função que executa o filtro
+    return pokemon.type.includes(tipoEscolhido) //includes compara elementos de outra array, pois type é uma array dentro da array pokeData. 
+  })
   console.log(filteredData)
 }
 
 
 
-
-document.getElementById("select-type").addEventListener("change", filterType) //verificando evento.
+//verificando eventos
+document.getElementById("select-type").addEventListener("change", filterType); //verificando evento.
 
