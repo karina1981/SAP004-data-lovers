d93c703215c648602abe29da55dd0e03abe04a76
Jessica Brunhara <brunharaj@gmail.com>
2020-05-15 11:11:31 -0300

    passando a função computeStates para o data.js

--- src/main.js
+++ src/main.js
@@ -7,8 +7,7 @@ const pokeData = data.pokemon;
 const rootElement = document.getElementById("root"); //importanto div do html
 const percentElement = document.getElementById("percent")
 
-//estrutura do card
-
+//escrevendo o html. estrutura do card
 const pokeCard = function (pokemon) {
   return `<div class="pokecard">
             <div class="pokeimg">
@@ -22,41 +21,32 @@ const pokeCard = function (pokemon) {
           </div>`
 };
 
-//função que gera e coloca o conteúdo na div. Os parametros dentro de pokeCard estão assim um embaixo do outro para melhor leitura. 
+//função que coloca o conteúdo do html na div. 
 const drawCard = function (pokemon) {
   rootElement.innerHTML += pokeCard(pokemon)
 };
 
-//mapeando todos os 151 pokemons e chamando a função que gera o card. Todos os pokemons de pokeData aparecem. 
+//map todos os 151 pokemons e chamando a função que gera o card. Todos os pokemons de pokeData aparecem. 
 pokeData.map(drawCard);
 
 
-//-----função filtrar por tipo------
+//-----função filtrar por tipo-----
 function filterType() {
   const tipoEscolhido = document.getElementById("select-type").value
 
-  const filteredData = search.filterData(pokeData, { 'key': 'type', 'value': tipoEscolhido}) 
+  const filteredData = search.filterData(pokeData, { 'key': 'type', 'value': tipoEscolhido })
 
   rootElement.innerHTML = '' //apaga todos os 151 cards que estavam aparecendo.
   filteredData.map(drawCard) //mapea os pokemons filtrados e chama a função drawCard, que gera o card.
 
-  // Testando cada pokemon paralimparCaixaPesquisa saber se ele é do tipo escolhido
-  let quantidadeTipoEscolhido = 0
-  for (let i = 0; i < pokeData.length; i++) {
-    if (pokeData[i].type.includes(tipoEscolhido)) {
-      // Se o pokemon é do tipo escolhido, adicione "1" a ele
-      quantidadeTipoEscolhido++
-
-    }
-  }
-  // Calculando a porcentagem desse tipo de pokemon dentro do total
-  let porcentagemFinal = quantidadeTipoEscolhido / pokeData.length * 100
-  porcentagemFinal = porcentagemFinal.toFixed(2)
+  //----calculando porcentagem----
+  const porcentagemFinal = search.computeStats(pokeData, tipoEscolhido)
 
   percentElement.innerHTML = `${porcentagemFinal}% dos Pokemons são do tipo ${tipoEscolhido}`
+
   limparCaixaPesquisa()
   limparOrder()
-  
+
 }
 
 //-------função filtra pelo nome buscado na caixa------
@@ -65,7 +55,7 @@ function filterSearchBox() {
   nameSearched = nameSearched.charAt(0).toUpperCase() + nameSearched.slice(1).toLowerCase(); //deixando a inicial maiúscula e o restante minúscula.
 
 
-  const filteredData = search.filterData(pokeData, { 'key': 'name', 'value': nameSearched}) 
+  const filteredData = search.filterData(pokeData, { 'key': 'name', 'value': nameSearched })
   percentElement.innerHTML = ""
   rootElement.innerHTML = ""
 
@@ -76,28 +66,28 @@ function filterSearchBox() {
 
 //-------função ordenar az za------------
 
-function OrderByAlphabet () {
+function OrderByAlphabet() {
   const orderDefined = document.getElementById("order-by-alphabet").value
 
   const orderedList = search.sortData(pokeData, orderDefined);
 
-  percentElement.innerHTML =""
-  rootElement.innerHTML = "" 
-  orderedList.map(drawCard) 
+  percentElement.innerHTML = ""
+  rootElement.innerHTML = ""
+  orderedList.map(drawCard)
   limparFiltrarTipo()
   limparCaixaPesquisa()
 }
 
 //------funções limpar------
-function limparOrder(){
+function limparOrder() {
   document.getElementById("order-by-alphabet").value = ""
 }
 
-function limparCaixaPesquisa(){
+function limparCaixaPesquisa() {
   document.getElementById("search-box").value = ""
 }
 
-function limparFiltrarTipo(){
+function limparFiltrarTipo() {
   document.getElementById("select-type").value = ""
 }
 
