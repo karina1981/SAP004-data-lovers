3c0ee6e748e0508d7bb3c26ed9a34cb2e2715f42
Karina Pereira de Souza <karina.souza1981@gmail.com>
2020-05-17 13:30:00 -0300

    modal

--- src/main.js
+++ src/main.js
@@ -10,7 +10,8 @@ const percentElement = document.getElementById("percent")
 //estrutura do card
 
 const pokeCard = function (pokemon) {
-  return `<div class="pokecard">
+  let pokemonStringFy = JSON.stringify(pokemon);
+  return `<div class="pokecard" data-pokemon='${pokemonStringFy}'>
             <div class="pokeimg">
               <img class='miniaturaPokemon' src="${pokemon.img}" alt ='miniatura pokemon'>
             </div>
@@ -21,6 +22,7 @@ const pokeCard = function (pokemon) {
             </div>
           </div>`
 };
+
 
 //função que gera e coloca o conteúdo na div. Os parametros dentro de pokeCard estão assim um embaixo do outro para melhor leitura. 
 const drawCard = function (pokemon) {
@@ -104,4 +106,53 @@ function limparFiltrarTipo(){
 //verificando eventos
 document.getElementById("select-type").addEventListener("change", filterType);
 document.getElementById("order-by-alphabet").addEventListener("change", OrderByAlphabet);
-document.getElementById("search-box").addEventListener("input", filterSearchBox)
+document.getElementById("search-box").addEventListener("input", filterSearchBox);
+
+document.getElementById("root").addEventListener("click", (event) => { 
+
+  if (event.target) {
+    let target = event.target;
+
+    let setPokecardDetails = (obj) => {
+      if (obj.className == 'pokecard') {
+
+        let pokemon = JSON.parse(obj.getAttribute('data-pokemon'));
+        
+        console.log('setPokecardDetails', pokemon);
+
+        let detailPokemon = `<div class="pokeimg">
+            <img class='miniaturaPokemon' src="${pokemon.img}" alt ='miniatura pokemon'>
+          </div>
+          <div class='pokelist'> 
+            Nome: ${pokemon.name} <br>
+            Tipo: ${pokemon.type} <br>
+            Tamanho: ${pokemon.height} <br>
+            Peso: ${pokemon.weight} <br>
+            Doces: ${pokemon.candy} <br>
+            Quantidade de Doces ${pokemon.candy_count} <br>
+            Choca em ovo ${pokemon.egg} <br>
+            Chance de Aparecer ${pokemon.spawn_chance} <br>
+            Média de Desova ${pokemon.avg_spawns} <br>
+            Tempo de Desova${pokemon.spawn_time} <br>
+            Multiplicadores ${pokemon.multipliers} <br>
+            Weaknesses ${pokemon.weaknesses} <br>
+          </div>`;
+console.log("detailPokemon")
+        let modal_background = document.querySelector('.modal_background');
+        modal_background.style.display = 
+        'flex'
+
+        let modalContent = document.querySelector('.modal_pokemon_content');
+        modalContent.innerHTML = detailPokemon;
+
+      } else {
+        if (obj.parentElement) {
+          return setPokecardDetails(obj.parentElement);
+        }
+      }
+    }
+
+    setPokecardDetails(target);
+
+  }
+});
